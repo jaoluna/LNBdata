e97b40cf5bbc1998470a2ad2ccf88d152690ce5a
joaoluna <joaovic135@gmail.com>
2023-07-11 13:08:33 -0300

    First commit: project initialization

---
+++ lbnAPI/src/main/java/com/lnb/monolith/lbnAPI/LbnApiApplication.java
@@ -0,0 +1,13 @@
+package com.lnb.monolith.lbnAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbnApiApplication.class, args);
+	}
+
+}
---
+++ lbnAPI/src/test/java/com/lnb/monolith/lbnAPI/LbnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnb.monolith.lbnAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
